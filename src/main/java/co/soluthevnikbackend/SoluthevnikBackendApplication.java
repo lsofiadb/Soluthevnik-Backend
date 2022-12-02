a6a99ff223a44896cd994920387eae630a2f23c6
Laura Sofia Dueñas Bulla <lauraduenas1105@gmail.com>
2022-12-02 12:38:20 -0500

    initial config

---
+++ src/main/java/co/soluthevnikbackend/SoluthevnikBackendApplication.java
@@ -0,0 +1,13 @@
+package co.soluthevnikbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoluthevnikBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoluthevnikBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/soluthevnikbackend/SoluthevnikBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package co.soluthevnikbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoluthevnikBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
