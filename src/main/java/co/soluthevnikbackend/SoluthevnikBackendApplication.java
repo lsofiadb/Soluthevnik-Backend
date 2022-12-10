16fac9cf042933527677d3a1250e1d1b86f5284b
Laura Sofia Dueñas Bulla <lauraduenas1105@gmail.com>
2022-12-10 14:25:55 -0500

    cors config

--- src/main/java/co/soluthevnikbackend/SoluthevnikBackendApplication.java
+++ src/main/java/co/soluthevnikbackend/SoluthevnikBackendApplication.java
@@ -2,6 +2,11 @@ package co.soluthevnikbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class SoluthevnikBackendApplication {
@@ -9,5 +14,18 @@ public class SoluthevnikBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SoluthevnikBackendApplication.class, args);
 	}
+	@Configuration
+	public class MyConfiguration {
 
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurerAdapter() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
 }
